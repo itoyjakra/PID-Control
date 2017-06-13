1028d803d02f29a90a8f0e5a8a3512cfe5b67a19
Arkajyoti Misra <arkajyoti@gmail.com>
2017-06-12 22:09:38 -0500

    First working PID controller with manual tuning

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,5 @@
 #include "PID.h"
+#include <iostream>
 
 using namespace std;
 
@@ -6,14 +7,28 @@ using namespace std;
 * TODO: Complete the PID class.
 */
 
-PID::PID() {}
+PID::PID() 
+{
+    p_error = 0;
+    i_error = 0;
+    d_error = 0;
+}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double kp, double ki, double kd) 
+{
+    Kp = kp;
+    Ki = ki;
+    Kd = kd;
+    std::cout << " K values in init: " << Kp << " " << Ki << " " << Kd << std::endl;
 }
 
-void PID::UpdateError(double cte) {
+void PID::UpdateError(double cte) 
+{
+    i_error += cte;
+    d_error = cte - p_error;
+    p_error = cte;
 }
 
 double PID::TotalError() {
--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,12 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
+  double kp = 0.1;
+  double ki = 0.0;
+  double kd = 0.6;
+  std::cout << " K values before init: " << pid.Kp << " " << pid.Ki << " " << pid.Kd << std::endl;
+  pid.Init(kp, ki, kd);
+  std::cout << " K values after init: " << pid.Kp << " " << pid.Ki << " " << pid.Kd << std::endl;
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -57,8 +63,13 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
+
+          pid.UpdateError(cte);
+          steer_value = - pid.Kp * pid.p_error - pid.Ki * pid.i_error - pid.Kd * pid.d_error;
           
           // DEBUG
+          std::cout << " K values: " << pid.Kp << " " << pid.Ki << " " << pid.Kd << std::endl;
+          std::cout << " error values: " << pid.p_error << " " << pid.i_error << " " << pid.d_error << std::endl;
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
           json msgJson;
